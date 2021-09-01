8486653a17fe598c94b0ddc2b9c18ef7ce71cecc
e-v-pavlova <catheripa@gmail.com>
2021-09-01 15:46:39 +0500

    feat: add index page for blog folder

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,7 @@
-<template>
-  <div>
-    <div
-      v-for="article in articles"
-      :key="article.slug"
-    >
-      <nuxt-link
-        :to="{ name:'blog-articleSlug', params: { articleSlug: article.slug } }"
-      >
-        <div>
-          <h2>{{ article.title }}</h2>
-          <p>{{ article.description }}</p>
-        </div>
-      </nuxt-link>
-    </div>
-  </div>
-</template>
-
 <script>
 export default {
-  async asyncData({ $content }) {
-    const articles = await $content('articles')
-      .only(['title', 'description', 'slug'])
-      .sortBy('createdAt', 'asc')
-      .fetch();
-    return {
-      articles,
-    };
+  middleware({ redirect }) {
+    return redirect('301', '/blog');
   },
 };
 </script>
