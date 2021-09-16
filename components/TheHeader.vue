aa2c4437e300b07de727ecdf29e3f5c47f7f818e
e-v-pavlova <catheripa@gmail.com>
2021-09-16 14:28:09 +0300

    feat: edit header

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,5 +1,26 @@
 <template>
   <header>
-    <search-input />
+    <nuxt-link
+      :to="'/'"
+    >
+      <svg
+        width="13.229mm"
+        height="13.229mm"
+        viewBox="0 0 13.229 13.229"
+      >
+        <g transform="translate(-107.91 -127.48)">
+          <circle
+            cx="114.53"
+            cy="134.09"
+            r="6.6146"
+            fill="#aeff00"
+            opacity=".74522"
+            style="paint-order:normal"
+          />
+        </g>
+      </svg>
+    </nuxt-link>
+    <div> Blog </div>
+    <social-buttons />
   </header>
 </template>
