8461b884c6d3cbd8a5edcab819f922674f3abdab
e-v-pavlova <catheripa@gmail.com>
2021-10-12 13:41:14 +0300

    feat: add purge setting to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  purge: [],
+  purge: [
+    './components/**/*.{vue,js}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+  ],
   darkMode: false,
   theme: {
     fontFamily: {
