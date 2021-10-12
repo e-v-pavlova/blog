7dac6a75ba0dad830bcc74a3834ecdf62063b2be
e-v-pavlova <catheripa@gmail.com>
2021-10-12 14:04:23 +0300

    feat: add share buttons styles

--- components/ShareButtons.vue
+++ components/ShareButtons.vue
@@ -1,17 +1,18 @@
 <!-- eslint-disable max-len -->
 <template>
-  <div>
+  <div class="flex flex-row text-black">
     <a
       :href="`https://www.facebook.com/sharer/sharer.php?u=${url}&t=${title}`"
       title="Facebook"
       target="_blank"
+      class="pr-3 transition-colors hover:text-green-persian"
     >
       <span>
         <svg
           width="11"
           height="20"
           viewBox="0 0 10.4 20"
-          fill="inherit"
+          fill="currentColor"
         >
           <path d="M6.7 20v-9.1h3.1l.5-3.6H6.7V5.1c0-1 .3-1.7 1.8-1.7h1.9V.1C10.1.1 8.9 0 7.6 0 4.9 0 3.1 1.7 3.1 4.7v2.6H0v3.6h3.1V20h3.6z" />
         </svg>
@@ -21,13 +22,14 @@
       :href="`https://twitter.com/share?url=${url}&text=${title}`"
       title="Twitter"
       target="_blank"
+      class="px-3 transition-colors hover:text-green-persian"
     >
       <span>
         <svg
           width="22"
           height="18"
           viewBox="0 0 22.1 18"
-          fill="inherit"
+          fill="currentColor"
         >
           <path d="M7 18c8.4 0 12.9-6.9 12.9-12.9v-.6c.9-.6 1.7-1.4 2.3-2.4-.8.4-1.7.6-2.6.7.9-.6 1.7-1.5 2-2.5-.9.5-1.9.9-2.9 1.1-.8-.9-2-1.4-3.3-1.4-2.5 0-4.5 2-4.5 4.5 0 .4 0 .7.1 1-3.8-.2-7.1-2-9.4-4.7-.4.7-.7 1.5-.7 2.3 0 1.6.8 3 2 3.8-.7 0-1.4-.2-2.1-.6v.1c0 2.2 1.6 4 3.6 4.5-.2 0-.6.1-1.1.1-.3 0-.6 0-.9-.1.6 1.8 2.3 3.1 4.2 3.2C5.2 15.3 3.2 16 1.1 16H0c2 1.2 4.4 2 7 2" />
         </svg>
@@ -37,13 +39,14 @@
       :href="`https://www.linkedin.com/shareArticle?mini=true&url=${url}&title=${title}`"
       title="LinkedIn"
       target="_blank"
+      class="px-3 transition-colors hover:text-green-persian"
     >
       <span>
         <svg
           width="20"
           height="20"
           viewBox="0 0 20 20"
-          fill="inherit"
+          fill="currentColor"
         >
           <path d="M.3 6.6h4.2V20H.3z" />
           <circle
@@ -59,13 +62,14 @@
       :href="`https://t.me/share/url?url=${url}&text=${title}`"
       title="Telegram"
       target="_blank"
+      class="pl-3 transition-colors hover:text-green-persian"
     >
       <span>
         <svg
           width="20"
           height="17"
           viewBox="0 0 20 16.5"
-          fill="inherit"
+          fill="currentColor"
         >
           <style>.st0{opacity:.2}</style>
           <path
