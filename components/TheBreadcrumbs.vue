2189546a275ace456ebc710c03a753a7137ef163
e-v-pavlova <catheripa@gmail.com>
2021-10-12 14:03:30 +0300

    feat: add breadcrumbs styles

--- components/TheBreadcrumbs.vue
+++ components/TheBreadcrumbs.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
+  <div
+    class="flex flex-row -mx-1 font-additional"
+  >
     <div
       v-for="(item, index) in items"
       :key="`breadcrumb-${index}`"
@@ -15,9 +17,17 @@
         :is="item.disabled || !item.url ? 'div' : 'nuxt-link' "
         v-else-if="item.name"
         :to="item.disabled || !item.url ? undefined : item.url"
+        :class="item.disabled || !item.url ? '' : 'hover:text-green-persian transition-colors'"
+        class="px-1 text-green-persian lg:text-current"
       >
         {{ item.name }}
       </nuxt-link>
+      <span
+        v-if="index !== items.length - 1"
+        class="pr-1"
+      >
+        /
+      </span>
     </div>
   </div>
 </template>
