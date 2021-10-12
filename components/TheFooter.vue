ce6bb3a4f0d01329fd93a98d7c80c187cecffc62
e-v-pavlova <catheripa@gmail.com>
2021-10-12 14:01:00 +0300

    feat: add footer styles

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,5 +1,7 @@
 <template>
-  <footer>
-    <div>&copy; {{ new Date().getFullYear() }} Blog</div>
+  <footer class="container flex justify-center min-w-full text-black">
+    <div class="px-2.5 sm:px-5 py-2.5 flex-1 flex items-center justify-between md:max-w-7xl">
+      &copy; {{ new Date().getFullYear() }} Blog
+    </div>
   </footer>
 </template>
