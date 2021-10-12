98504477e2db9420701ab5c492b511b570d5bb72
e-v-pavlova <catheripa@gmail.com>
2021-10-12 14:04:52 +0300

    feat: add article list styles

--- components/ArticleList.vue
+++ components/ArticleList.vue
@@ -1,15 +1,25 @@
 <template>
-  <div>
+  <div class="divide-y divide-gray-200">
     <div
       v-for="article of articles"
       :key="article.slug"
     >
       <nuxt-link
         :to="{ name: 'blog-articleSlug', params: { articleSlug: article.slug } }"
+        class="flex flex-col py-5 cursor-pointer group lg:py-8 md:flex-row"
       >
-        <div>
-          <h2>{{ article.title }}</h2>
-          <p>{{ article.description }}</p>
+        <div class="flex-shrink-0 mb-3 md:w-52 md:mb-0 sm:mb-6 lg:pt-1">
+          <span class="text-sm text-gray-400 lg:text-base">
+            {{ formatDate(article.createdAt) }}
+          </span>
+        </div>
+        <div class="md:flex-grow">
+          <h2 class="mb-2 text-gray-900 transition-colors group-hover:text-green-persian">
+            {{ article.title }}
+          </h2>
+          <p class="leading-relaxed">
+            {{ article.description }}
+          </p>
         </div>
       </nuxt-link>
     </div>
@@ -22,6 +32,12 @@ export default {
     articles: {
       type: Array,
       default: () => [],
+    },
+  },
+  methods: {
+    formatDate(date) {
+      const options = { year: 'numeric', month: 'short', day: 'numeric' };
+      return new Date(date).toLocaleDateString('en-GB', options);
     },
   },
 };
