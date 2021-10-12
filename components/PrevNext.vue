7dcec650789f2470fc56ea3cfbf3577af0ef4bcf
e-v-pavlova <catheripa@gmail.com>
2021-10-12 14:03:01 +0300

    feat: add prev/next links styles

--- components/PrevNext.vue
+++ components/PrevNext.vue
@@ -1,17 +1,33 @@
 <template>
-  <div>
+  <div class="flex flex-col justify-between lg:flex-row">
     <nuxt-link
       v-if="prev"
       :to="{ name: 'blog-articleSlug', params: { articleSlug: prev.slug } }"
+      class="pr-3 my-2 group"
     >
-      {{ prev.title }}
+      <div class="text-base text-gray-400">
+        &lt; Previous Post
+      </div>
+      <div
+        class="transition-colors text-green-persian lg:text-current group-hover:text-green-persian"
+      >
+        {{ prev.title }}
+      </div>
     </nuxt-link>
     <span v-else>&nbsp;</span>
     <nuxt-link
       v-if="next"
       :to="{ name: 'blog-articleSlug', params: { articleSlug: next.slug } }"
+      class="pl-3 my-2 text-right group"
     >
-      {{ next.title }}
+      <div class="text-base text-gray-400">
+        Next Post &gt;
+      </div>
+      <div
+        class="transition-colors text-green-persian lg:text-current group-hover:text-green-persian"
+      >
+        {{ next.title }}
+      </div>
     </nuxt-link>
     <span v-else>&nbsp;</span>
   </div>
