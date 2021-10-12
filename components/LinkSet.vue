578531092453f7f7ef8f78e96cae7b91bb55bb76
e-v-pavlova <catheripa@gmail.com>
2021-10-12 14:01:50 +0300

    feat: add link set styles

--- components/LinkSet.vue
+++ components/LinkSet.vue
@@ -1,12 +1,16 @@
 <template>
-  <div>
+  <div
+    class="flex flex-row"
+  >
     <nuxt-link
       :is="item.url ? 'nuxt-link' : 'div'"
       v-for="(item, index) in items"
       :key="`link-${index}`"
       :to="item.url ? item.url : undefined"
+      :class="item.url ? 'hover:text-green-persian transition-colors' : ''"
+      class="px-1 text-green-persian lg:text-current"
     >
-      {{ item.name }}
+      {{ `${item.name}${index !== items.length - 1 ? ',' : ''}` }}
     </nuxt-link>
   </div>
 </template>
